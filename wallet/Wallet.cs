aa9dd0febbacf8c2508a48e86e739cd5b5007081
Leons Strauß <leonstrauss99@gmail.com>
2020-02-28 10:16:07 +0100

    updatet

--- wallet/Wallet.cs
+++ wallet/Wallet.cs
@@ -17,11 +17,6 @@ namespace wallet
         {
         }
 
-        public void TransferTo(Money amount, IDeposit toWallet)
-        {
-            toWallet.Deposit(this.Withdraw(amount));
-        }
-
         public Money Withdraw(Money amount)
         {
             return this.Balance.Subtract(amount);
